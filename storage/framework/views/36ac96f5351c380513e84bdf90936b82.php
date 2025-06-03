bef7daf8e613aaad2c542a864d78a256855d52fa
Sherif Emad <sherifemad292@gmail.com>
2025-06-03 23:37:47 +0300

    edits on gitnore

---
+++ storage/framework/views/36ac96f5351c380513e84bdf90936b82.php
@@ -0,0 +1,21 @@
+<div class="row">
+    <div class="col-12 mb-2 text-center">
+        <?php echo HTML::icon_link(route('social.redirect',['provider' => 'facebook']), 'fa-brands fa-facebook', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-facebook')); ?>
+
+        <?php echo HTML::icon_link(route('social.redirect',['provider' => 'twitter']), 'fa-brands fa-twitter', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-twitter')); ?>
+
+        <?php echo HTML::icon_link(route('social.redirect',['provider' => 'google']), 'fa-brands fa-google-plus', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-google')); ?>
+
+        <?php echo HTML::icon_link(route('social.redirect',['provider' => 'github']), 'fa-brands fa-github', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-github')); ?>
+
+        <?php echo HTML::icon_link(route('social.redirect',['provider' => 'youtube']), 'fa-brands fa-youtube', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-youtube')); ?>
+
+        <?php echo HTML::icon_link(route('social.redirect',['provider' => 'twitch']), 'fa-brands fa-twitch', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-twitch')); ?>
+
+        <?php echo HTML::icon_link(route('social.redirect',['provider' => 'instagram']), 'fa-brands fa-instagram', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-instagram')); ?>
+
+        <?php echo HTML::icon_link(route('social.redirect',['provider' => '37signals']), 'fa fa-signal', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-basecamp')); ?>
+
+    </div>
+</div>
+<?php /**PATH C:\xampp\htdocs\DUBBATECH\Ruyas-app\resources\views/partials/socials-icons.blade.php ENDPATH**/ ?>
